7d3c655f15bb926bdb67fc73bdf7be2bc5907ad6
Aklilu Mandefro <hello@aklilumandefro.com>
2024-12-27 00:44:08 +0300

    Added a try catch block

--- server/user-routes.js
+++ server/user-routes.js
@@ -5,12 +5,10 @@ const db = require("./database/connection.js");
 
 //Create a new post route that handles the incoming user
 // registration requests
-
 router.post("/", async (req, res) => {
   const { username, password, email } = req.body;
 
   // Validate user input
-
   if (!username || !password || !email) {
     return res.status(400).json({
       error: "Username, password, and email are required.",
@@ -18,26 +16,32 @@ router.post("/", async (req, res) => {
   }
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  // Inserting user's data into the database
-  const [id] = await db("users").insert({
-    username,
-    password: hashedPassword,
-    email,
-  });
-
-  // Respond with the created user data
-  res.status(201).json({
-    id,
-    username,
-    email,
-  });
-
-  //Check for unique constraint violation
-  if (error.code === 23505) {
-    return res.status(409).json({
-      error: " Username or email already exist.",
+  try {
+    // Inserting user's data into the database
+    const [id] = await db("users").insert({
+      username,
+      password: hashedPassword,
+      email,
     });
+
+    // Respond with the created user data
+    res.status(201).json({
+      id,
+      username,
+      email,
+    });
+  } catch (error) {
+    //Check for unique constraint violation
+    if (error.code === 23505) {
+      return res.status(409).json({
+        error: " Username or email already exist.",
+      });
+    }
   }
+
+  res.status(500).json({
+    error: "User registration failed.",
+  });
 });
 
 module.exports = router;
