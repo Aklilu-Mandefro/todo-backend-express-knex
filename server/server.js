7fc35cdd3f0c8f3689b4e0827b70fe5b0e21b3b4
Aklilu Mandefro <hello@aklilumandefro.com>
2024-12-27 00:48:16 +0300

    Imported user registration routes

--- server/server.js
+++ server/server.js
@@ -1,18 +1,22 @@
-const app = require('./server-config.js');
-const routes = require('./server-routes.js');
+const app = require("./server-config.js");
+const routes = require("./server-routes.js");
+const userRoutes = require("./user-routes.js"); //Import the user registration routes
 
 const port = process.env.PORT || 5000;
 
-app.get('/', routes.getAllTodos);
-app.get('/:id', routes.getTodo);
+app.get("/", routes.getAllTodos);
+app.get("/:id", routes.getTodo);
 
-app.post('/', routes.postTodo);
-app.patch('/:id', routes.patchTodo);
+app.post("/", routes.postTodo);
+app.patch("/:id", routes.patchTodo);
 
-app.delete('/', routes.deleteAllTodos);
-app.delete('/:id', routes.deleteTodo);
+app.delete("/", routes.deleteAllTodos);
+app.delete("/:id", routes.deleteTodo);
 
-if (process.env.NODE_ENV !== 'test') {
+// New user registration route
+app.use("/api/users", userRoutes);
+
+if (process.env.NODE_ENV !== "test") {
   app.listen(port, () => console.log(`Listening on port ${port}`));
 }
 
